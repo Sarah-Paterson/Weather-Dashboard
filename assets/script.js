0dc14c873f815267cd46a5011f00c3fa48c831f7
kitkatyourmom <sarahlridings@gmail.com>
2023-05-19 21:13:58 -0600

    add HTML variables in JS

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+let cityInputElement = document.querySelector('#city-input');
+let weatherContainerElement = document.querySelector('#weater-container');
+let weatherTodayElement = document.querySelector('#weater-today');
+let fiveDayContainerElement = document.querySelector('#five-day-container');
+
